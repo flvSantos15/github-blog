33a62879f49aa0168e28963eca37f3c22c8c29f0
flvSantos15 <flvSantos300@gmail.com>
2023-01-29 23:53:10 -0300

    feat(feature/header): header finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,23 @@ import rightImg from '../assets/effect-right.png'
 
 export function Header() {
   return (
-    <div className="flex items-center justify-between absolute w-full xl:h-[296px] bg-base-profile">
+    <div className="flex items-center justify-center md:justify-between xl:justify-between absolute w-full h-[120px] md:h-[276px] xl:h-[296px] bg-base-profile">
       {/* <div className="flex justify-between absolute xl:max-w-[1440px] xl:h-[296px] bg-base-profile"> */}
-      <img src={rightImg} alt="" />
-      <img src={logoImg} alt="" className="w-[148px] h-[98px]" />
-      <img src={leftImg} alt="" />
+      <img
+        src={rightImg}
+        alt=""
+        className="hidden md:flex xl:flex md:w-[270px] xl:w-[409px]"
+      />
+      <img
+        src={logoImg}
+        alt=""
+        className="w-[98px] md:w-[128px] xl:w-[148px] md:h-[78px] xl:h-[98px]"
+      />
+      <img
+        src={leftImg}
+        alt=""
+        className="hidden md:flex xl:flex md:w-[271px] xl:w-[371px]"
+      />
       {/* </div> */}
     </div>
   )
