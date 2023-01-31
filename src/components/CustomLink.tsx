fce4df08b3614c941d3e2772b5152fc763dad19c
flvSantos15 <flvSantos300@gmail.com>
2023-01-31 16:28:48 -0300

    feat: getting data from github api

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import { FiExternalLink } from 'react-icons/fi'
 
 interface CustomLinkProps {
   label: string
+  url: string
 }
 
-export function CustomLink({ label }: CustomLinkProps) {
+export function CustomLink({ label, url }: CustomLinkProps) {
   return (
-    <div className="flex items-center gap-1 p-0">
-      <span className="font-['Nunito'] font-bold text-xs leadgin-[19px] uppercase text-blue hover:underline">
-        {label}
-      </span>
-      <FiExternalLink color="#3294F8" />
-    </div>
+    <NavLink to={url}>
+      <div className="flex items-center gap-1 p-0">
+        <span className="font-['Nunito'] font-bold text-xs leadgin-[19px] uppercase text-blue hover:underline">
+          {label}
+        </span>
+        <FiExternalLink color="#3294F8" />
+      </div>
+    </NavLink>
   )
 }
